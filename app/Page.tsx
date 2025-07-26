234b7a8d9408b392bf295381515aeb8860443f41
Hdstec33 <herculesdorneles@gmail.com>
2025-07-26 10:20:21 -0300

    Create Page.tsx

---
+++ app/Page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-green-500 flex items-center justify-center">
+      <h1 className="text-white text-6xl font-bold">
+        TESTE SIMPLES FUNCIONANDO!
+      </h1>
+    </div>
+  )
+}
